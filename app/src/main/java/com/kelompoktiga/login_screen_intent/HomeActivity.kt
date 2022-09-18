15edad628230f82055ec8eaa20159644ee1d6d8c
Andrew Virya Victorio <andrew.avv03@gmail.com>
2022-09-18 16:22:21 +0700

    feat: pass username data to home activity

--- app/src/main/java/com/kelompoktiga/login_screen_intent/HomeActivity.kt
+++ app/src/main/java/com/kelompoktiga/login_screen_intent/HomeActivity.kt
@@ -1,11 +1,23 @@
 package com.kelompoktiga.login_screen_intent
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        val logoutButton: Button = findViewById(R.id.btnLogout)
+        val user: TextView = findViewById(R.id.viewUser)
+        val intent: Intent = this.intent
+        val bundle: Bundle? = intent.extras
+
+        if (bundle != null) {
+            user.text = String.format("Welcome, %s", bundle.getString("Username"))
+        }
     }
 }
